800684fd69bbdb67f30d6d14b67ed7777123d923
SirBucketT <tkpriva@icloud.com>
2024-12-05 16:53:38 +0100

    exp & MP modifications through scriptable object

--- Assets/Scripts/UI/Player/MPManager.cs
+++ Assets/Scripts/UI/Player/MPManager.cs
@@ -4,6 +4,7 @@ using UI.Player;
 using UnityEngine;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
+using TMPro;
 
 public class MPManager : MonoBehaviour
 { 
@@ -15,6 +16,10 @@ public class MPManager : MonoBehaviour
     private float MP;
     private readonly float _lerpSpeed = 0.05f;
     
+    
+    [SerializeField] private TMP_Text currentExpDisplay;
+    [SerializeField] private TMP_Text maxMpDisplay;
+    
     void Start()
     {
         maxHealth = MpData.maxMp;
@@ -23,6 +28,9 @@ public class MPManager : MonoBehaviour
 
     void Update()
     {
+        maxMpDisplay.text = MpData.maxMp.ToString();
+        currentExpDisplay.text = MpData.mp.ToString();
+
         //slider code, do not touch
         if (playerMpbar.value != MP){
             playerMpbar.value = MP;
