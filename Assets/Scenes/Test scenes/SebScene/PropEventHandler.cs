28fe71c186892f9491db2425e5274ad36f600430
CyborgDitz <sebban.lol@gmail.com>
2024-12-05 20:51:33 +0100

    Working on implementing Input+Collision destroy object

--- Assets/Scenes/Test scenes/SebScene/PropEventHandler.cs
+++ Assets/Scenes/Test scenes/SebScene/PropEventHandler.cs
@@ -3,23 +3,44 @@ using UnityEngine.Events;
 
 public class PropEventHandler : MonoBehaviour
 {
-    public UnityEvent onCollisonEvent;
+    [SerializeField] private KeyCode _playerInteractKet= KeyCode.D;
+    
+    public UnityEvent onCollisionEvent;
+    public UnityEvent onKeyPressEvent;
+    private bool isPlayerInRange = false;  
+    private bool isKeyPressed = false;     
     private void Start()
     {
-        onCollisonEvent ??= new UnityEvent();
+        onCollisionEvent ??= new UnityEvent();
+        onKeyPressEvent ??= new UnityEvent();
     }
 
-    private void TriggerDisableEvent()
+    private void Update()
     {
-        Debug.Log("Disable event triggered!");
-        onCollisonEvent.Invoke();
+        if (Input.GetKeyDown(_playerInteractKet))
+        {
+            isKeyPressed = true;
+            TriggerKeyPressEvent();
+        }
+    }
+
+    private void TriggerCollisionEvent()
+    {
+        Debug.Log("Collision event triggered!");
+        onCollisionEvent.Invoke();
+    }
+
+    private void TriggerKeyPressEvent()
+    {
+        Debug.Log("Key press event triggered!");
+        onKeyPressEvent.Invoke();
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            TriggerDisableEvent();
+            TriggerCollisionEvent();
         }
     }
 }
