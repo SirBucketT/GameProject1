617d1f8166ec025fb8a3dafb5253781ce023e9c8
SirBucketT <tkpriva@icloud.com>
2024-12-05 11:03:19 +0100

    Experiance bar UI changed
    Modified the look of the games experiance bar in the UI to fit in with the healthbar and MP bar. Modified script to enable for leveling up player character and natural progression of exp curve for each new level gained

--- Assets/Scripts/UI/Player/EXPManager.cs
+++ Assets/Scripts/UI/Player/EXPManager.cs
@@ -5,44 +5,63 @@ using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class EXPManager : MonoBehaviour
-{ 
-    [SerializeField] private Slider playerExpBar; 
+{
+    [SerializeField] private Slider playerExpBar;
     [SerializeField] private Slider playerExpBarBack;
     [SerializeField] private float maxHealth = 100;
-    private float _exp;
+    [SerializeField] private float minHealth = 100;
+    public float exp;
     private readonly float _lerpSpeed = 0.05f;
-    
+
+    public int playerLevel = 100;
+
     private bool _isPlayerExpFull;
-    
+
     void Start()
     {
-        _exp = maxHealth;
+        exp = minHealth;
         _isPlayerExpFull = false;
+        playerLevel = 1; //SO_levelManager.CurrentLevel();
     }
 
     void Update()
     {
         //slider code, do not touch
-        if (playerExpBar.value != _exp){
-            playerExpBar.value = _exp;
+        if (playerExpBar.value != exp)
+        {
+            playerExpBar.value = exp;
         }
-        if (Input.GetKeyUp(KeyCode.S)) {
+
+        if (Input.GetKeyUp(KeyCode.S))
+        {
             GainExperiancePoints(10);
         }
-        if (playerExpBar.value != playerExpBarBack.value) {
-            playerExpBarBack.value = Mathf.Lerp(playerExpBarBack.value, _exp, _lerpSpeed); 
+
+        if (playerExpBar.value != playerExpBarBack.value)
+        {
+            playerExpBarBack.value = Mathf.Lerp(playerExpBarBack.value, exp, _lerpSpeed);
         }
-        if (_exp <= 0 && !_isPlayerExpFull) {
+
+        if (exp >= maxHealth && !_isPlayerExpFull)
+        {
             _isPlayerExpFull = true;
-        //     TODO implement feature that loads MP empty UI elements
+            //     TODO implement feature that loads MP empty UI elements
         }
-    }
-    
-    // ReSharper disable Unity.PerformanceAnalysis
-    void GainExperiancePoints(float expGain) {
-        _exp += expGain;
-        Debug.Log("S pressed, you gained 10 EXP");
-        Debug.Log($"Player EXP: {_exp}");
     }
 
+    // ReSharper disable Unity.PerformanceAnalysis
+    void GainExperiancePoints(float expGain)
+    {
+        exp += expGain;
+        Debug.Log("S pressed, you gained 10 EXP");
+        Debug.Log($"Player EXP: {exp}");
+
+        if (exp >= maxHealth)
+        {
+            playerLevel += 1;
+            maxHealth *= 2;
+            exp = minHealth;
+        }
+
+    }
 }
