1515f756e86a02524b9ce3ed14c2a30941b08f33
sibjor <127234354+sibjor@users.noreply.github.com>
2024-12-09 14:39:48 +0100

    SixtenDirectory

---
+++ Assets/Scenes/Test scenes/SixtenScene/Player/scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using Unity.VisualScripting;
+using UnityEditor.Animations;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Timeline;
+
+public class PlayerMovement : MonoBehaviour
+{
+    RaycastHit hitInfo = new();
+    NavMeshAgent agent;
+    private Animator animator;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float normalizedSpeed = agent.velocity.magnitude / agent.speed;
+        animator.SetFloat("Velocity", normalizedSpeed);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Single Click
+            if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+            {
+                agent.destination = hitInfo.point;
+            }
+        }
+
+        if (Input.GetMouseButton(0)) // Hold
+        {
+            if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+            {
+                agent.destination = hitInfo.point;
+            }
+        }
+    }
+}
