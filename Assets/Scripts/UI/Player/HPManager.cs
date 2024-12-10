b459dfc67c4c33300ff50b281b72039acdbb892c
SirBucketT <tkpriva@icloud.com>
2024-12-10 15:45:50 +0100

    method update

--- Assets/Scripts/UI/Player/HPManager.cs
+++ Assets/Scripts/UI/Player/HPManager.cs
@@ -1,9 +1,11 @@
+using Enemy;
 using UnityEngine;
 using UI.Player;
 
 [CreateAssetMenu(fileName = "HP Manager", menuName = "ScriptableObjects/HP Manager")]
 public class HPManager : ScriptableObject
 {
+    public EnemyHealthbar EnemyHealthbar;
     public float maxHP;
     public float currentHP;
     
@@ -11,6 +13,7 @@ public class HPManager : ScriptableObject
         currentHP -= damage;
         Debug.Log("spaceBar pressed, you lose 10 health");
         Debug.Log($"Player health: {currentHP}");
+        EnemyHealthbar.UpdateEnemyHealth();
 
         if (currentHP <= 0)
         {
