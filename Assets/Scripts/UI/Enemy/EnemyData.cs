ab71ab512a5fc0793f3b424b772941d72f745463
SirBucketT <tkpriva@icloud.com>
2024-12-10 17:18:08 +0100

    patching things around a bit

--- Assets/Scripts/UI/Enemy/EnemyData.cs
+++ Assets/Scripts/UI/Enemy/EnemyData.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "EnemyData", menuName = "Scriptable Objects/EnemyData")]
 public class EnemyData : ScriptableObject
 {
-    private EnemyHealthbar healthbar;
+    private EnemyHealthbar _healthbar;
     
     [SerializeField] public float CurrentHealth;
     [SerializeField] public float MaxHealth;
     
     public void TakeDamage(float damage) {
         CurrentHealth -= damage;
-        healthbar.UpdateEnemyHealth();
+        _healthbar.UpdateEnemyHealth();
+        Debug.Log($"Enemy damaged: {damage}");
     }
 }
