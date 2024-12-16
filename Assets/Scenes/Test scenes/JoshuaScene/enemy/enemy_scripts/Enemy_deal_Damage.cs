f198b429d22c6b2ef42adc20e2463cba6e54c073
Joshua Aroka <josaro8260@gmail.com>
2024-12-16 16:07:36 +0100

    update

---
+++ Assets/Scenes/Test scenes/JoshuaScene/enemy/enemy_scripts/Enemy_deal_Damage.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+
+public class Enemy_deal_Damage : MonoBehaviour
+{
+    public int damage = 5;
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<PlayerTakeDamage>().HpManager.PlayerTakeDamage(10);
+        }
+    }
+}
