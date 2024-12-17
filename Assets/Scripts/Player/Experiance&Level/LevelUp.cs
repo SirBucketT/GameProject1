6a1c2ea3d86d828d8312432b2b110dd8028b7e01
CyborgDitz <sebban.lol@gmail.com>
2024-12-17 14:05:51 +0100

    Revert "Merge branch 'main' of https://github.com/SirBucketT/GameProject1"
    This reverts commit 9e5ad3f6f207bd844edbb9bfe965ce09ea294d58, reversing
    changes made to 4babd84c6f4f1ab406593373bdc62624d83d2e56.

--- Assets/Scripts/Player/Experiance&Level/LevelUp.cs
+++ Assets/Scripts/Player/Experiance&Level/LevelUp.cs
@@ -5,18 +5,14 @@ public class LevelUp : MonoBehaviour
     [SerializeField] PlayerData playerData;
     [SerializeField] HealthbarManager hpBar;
     
-    void Update()
+    void LateUpdate()
     {
         if (playerData.currentExp >= playerData.maxExp)
         {
             playerData.currentExp = playerData.minExp;
             playerData.maxExp *= 1.3f;
             playerData.playerLevel += 1;
-            
             playerData.maxHealth *= 1.7f;
-            playerData.maxMp *= 1.8f;
-            
-            playerData.currentMp = playerData.maxMp;
             playerData.currentHealth = playerData.maxHealth;
             hpBar.UpdateHealthBarMaxValue();
         }
