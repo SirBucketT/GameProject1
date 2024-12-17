5b82bc9c26171cd4cafdda31614414ffddce5f31
SirBucketT <tkpriva@icloud.com>
2024-12-17 14:29:11 +0100

    GameUI Prefab modifed with refactoried SO code

--- Assets/Scripts/UI/Player/GoldUI.cs
+++ Assets/Scripts/UI/Player/GoldUI.cs
@@ -4,15 +4,11 @@ using TMPro;
 
 public class GoldUI : MonoBehaviour
 {
-    public Gold gold; 
+    [SerializeField] PlayerData gold; 
     [SerializeField] private TMP_Text goldText;
 
     void Update()
     {
         goldText.text = gold.gold.ToString();
-        
-        if (Input.GetKeyUp(KeyCode.B)) {
-            gold.GetGold(10);
-        }
     }
 }
