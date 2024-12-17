370ce442045746b1c4c9b1244527e1a3007d45dc
Joshua Aroka <josaro8260@gmail.com>
2024-12-17 15:11:58 +0100

    Player_take_damage_80%_done

--- Assets/Scenes/Test scenes/JoshuaScene/enemy/enemy_scripts/Enemy_deal_Damage.cs
+++ Assets/Scenes/Test scenes/JoshuaScene/enemy/enemy_scripts/Enemy_deal_Damage.cs
@@ -1,14 +1,15 @@
-// using System;
-// using UnityEngine;
-//
-// public class Enemy_deal_Damage : MonoBehaviour
-// {
-//     public int damage = 5;
-//     private void OnCollisionEnter(Collision other)
-//     {
-//         if (other.gameObject.CompareTag("Player"))
-//         {
-//             other.gameObject.GetComponent<PlayerTakeDamage>().HpManager.PlayerTakeDamage(10);
-//         }
-//     }
-// }
+using System;
+using UnityEngine;
+
+public class Enemy_deal_Damage : MonoBehaviour
+{
+    public int damage = 5;
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<PlayerHealth>().PlayerTakeDamage(damage);
+            Debug.Log($"Enemy dealt {damage} to Player!");
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    private TakeDamageManager _takeDamageManager;
+    private float playerHealth;
+    [SerializeField] PlayerData _playerData;
+
+    public void PlayerTakeDamage(float damage)
+    {
+        _playerData.currentHealth -= damage;
+        Debug.Log($"Player health {_playerData.currentHealth}");
+
+    }
+}
