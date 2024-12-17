0b85620b0c11ef458e4026c21074dee8c1142232
CyborgDitz <sebban.lol@gmail.com>
2024-12-17 13:44:33 +0100

    Update for death

--- Assets/Scripts/TakeDamageManager.cs
+++ Assets/Scripts/TakeDamageManager.cs
@@ -1,11 +1,17 @@
+using System.Collections;
 using UnityEngine;
 
 public class TakeDamageManager : MonoBehaviour
-{ 
+{
     internal bool _isAlive;
     internal int _maxHealth; 
     internal int _currentHealth;
-
+  
+    private bool _isDestroyed = false;
+    private bool _isDisabled;
+    
+    private static int destroyedCount = 0;
+    private static int disabledCount = 0;
     public void TakeDamage(int damageAmount)
     {
         if (!_isAlive) return;
@@ -22,5 +28,37 @@ public class TakeDamageManager : MonoBehaviour
     {
         _isAlive = false;
         Debug.Log($"{gameObject} has died.");
+        _isDestroyed = true;
+        DisableObject();
+    }
+    private void DisableObject()
+    {
+        if (_currentHealth <= 0 && !_isAlive && !_isDisabled) 
+        {
+            _isDisabled = true;
+            
+            gameObject.SetActive(false);
+            disabledCount++;
+            Debug.Log($"Enemy disabled (SetActive false). Total disabled count: {disabledCount}. GameObject: {gameObject.name}");
+            
+            if (this != null && gameObject.activeSelf)
+            {
+                StartCoroutine(DestroyLater());
+            }
+        }
+    }
+    
+    private IEnumerator DestroyLater()
+    {
+        yield return new WaitForSeconds(5f);
+    
+        if (gameObject != null && !_isDestroyed)
+        {
+            Destroy(this.gameObject);
+            destroyedCount++;  
+            Debug.Log($"Enemy destroyed (GameObject destroyed). " +
+                      $"Total destroyed count: {destroyedCount}. GameObject: {gameObject.name}");
+            _isDestroyed = true;
+        }
     }
 }
