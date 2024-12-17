f3918462bcb2b1e46c0b484d326ce6368bc44457
SirBucketT <tkpriva@icloud.com>
2024-12-17 11:56:33 +0100

    refactoring update

--- Assets/Scripts/Player/PlayerData.cs
+++ Assets/Scripts/Player/PlayerData.cs
@@ -3,17 +3,22 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PlayerData", menuName = "Scriptable Objects/PlayerData")]
 public class PlayerData : ScriptableObject
 {
+    [Header("Player Money")]
     public int gold;
     
+    [Header("Player Mana")]
     public float currentMp;
     public float maxMp;
     
+    [Header("Player health")]
     public float maxHealth;
     public float currentHealth;
 
+    [Header("Player Level")]
     public int playerLevel;
     public int playerMaxLevel;
 
+    [Header("Player experience")]
     public float minExp;
     public float maxExp;
     public float currentExp;
