e2037bbabde715933cfbcf6eee26c774d4b5c0d0
SirBucketT <tkpriva@icloud.com>
2024-12-18 14:02:58 +0100

    dialogue is working, sort of

--- Assets/Scripts/UI/NPC/Dialogue.cs
+++ Assets/Scripts/UI/NPC/Dialogue.cs
@@ -1,8 +1,14 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
+[System.Serializable]
 public class Dialogue
 {
     public string name;
+    
+    [TextArea(1, 6)]
+    
     public string[] sentences;
     
 }
--- Assets/Scripts/UI/NPC/DialogueManager.cs
+++ Assets/Scripts/UI/NPC/DialogueManager.cs
@@ -1,14 +1,65 @@
-using UnityEngine;
-using System.Collections.Generic;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-public class DialogueManager : MonoBehaviour
-{
-    
+public class DialogueManager : MonoBehaviour {
+
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+
+    public Animator animator;
+
     private Queue<string> _sentences;
-    
-    void Start()
-    {
+
+    // Use this for initialization
+    void Start () {
         _sentences = new Queue<string>();
     }
+
+    public void StartDialogue (Dialogue dialogue)
+    {
+        animator.SetBool("IsOpen", true);
+
+        nameText.text = dialogue.name;
+
+        _sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            _sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence ()
+    {
+        if (_sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = _sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    void EndDialogue()
+    {
+        animator.SetBool("IsOpen", false);
+    }
+
 }
