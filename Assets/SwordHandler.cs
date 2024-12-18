640e702fd587882760836e7d32cc5003b61edbdc
sibjor <127234354+sibjor@users.noreply.github.com>
2024-12-18 09:42:27 +0100

    Separation of Concerns

---
+++ Assets/SwordHandler.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class SwordHandler : MonoBehaviour
+{
+        [SerializeField] GameObject backAttachPoint;
+        [SerializeField] GameObject rightHandAttachPoint;
+        [SerializeField] GameObject sword;
+        private Animator animator;
+        
+    
+        private bool isSwordInHand = false;
+
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+        }
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                ToggleSword();
+            }
+        }
+    
+        void ToggleSword()
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                animator.SetTrigger("Sword");
+            }
+        }
+}
