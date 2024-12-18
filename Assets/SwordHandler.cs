af93472b64141dab5314220af83857aff79b8761
sibjor <127234354+sibjor@users.noreply.github.com>
2024-12-18 15:24:16 +0100

    DrawSword

--- Assets/SwordHandler.cs
+++ Assets/SwordHandler.cs
@@ -2,31 +2,46 @@ using UnityEngine;
 
 public class SwordHandler : MonoBehaviour
 {
-        [SerializeField] GameObject backAttachPoint;
-        [SerializeField] GameObject rightHandAttachPoint;
-        [SerializeField] GameObject sword;
-        private Animator animator;
-        
-    
-        private bool isSwordInHand = false;
+    [SerializeField] GameObject backAttachPoint;
+    [SerializeField] GameObject rightHandAttachPoint;
+    [SerializeField] GameObject sword;
+    private Animator animator;
+    private float animationTimer;
+    private float switchPoint;
 
-        void Start()
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            animator = GetComponent<Animator>();
+            ToggleSword();
         }
-        void Update()
+    }
+
+    void ToggleSword()
+    {
+        if (!animator.GetBool("SwordDrawn"))
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            animator.SetBool("SwordDrawn", true);
+            sword.transform.SetParent(rightHandAttachPoint.transform);
+            sword.transform.localPosition = new Vector3(0.2f, 0, 0);
+            sword.transform.localRotation = Quaternion.Euler(0, 0, -300);
+        }
+        else
+        {
+            animator.SetBool("SwordDrawn", false);
+            ;
+            if (animator.GetFloat("Speed") >= 0.1)
             {
-                ToggleSword();
+                sword.transform.SetParent(backAttachPoint.transform);
             }
+            
         }
+    }
     
-        void ToggleSword()
-        {
-            if (Input.GetKeyDown("space"))
-            {
-                animator.SetTrigger("DrawSword");
-            }
-        }
+    
 }
