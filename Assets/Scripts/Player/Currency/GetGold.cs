b1c7b5e29ebd0904cd97d31ddd611646fe97b934
SirBucketT <tkpriva@icloud.com>
2024-12-27 01:48:03 +0100

    inactivity tutorial in main scene implemented

--- Assets/Scripts/Player/Currency/GetGold.cs
+++ Assets/Scripts/Player/Currency/GetGold.cs
@@ -3,4 +3,8 @@ using UnityEngine;
 public class GetGold : MonoBehaviour
 {
     [SerializeField] PlayerData playerData;
+    
+    public void GetCash(int amount) {
+        playerData.gold += amount;
+    }
 }
---
+++ Assets/Scripts/UI/InactivityChecker.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class InactivityChecker : MonoBehaviour
+{
+    [SerializeField] private float secondsToWait = 5f; 
+    private float inactivityTimer = 0f;
+    private bool hasBeenInactive = true;
+
+    [SerializeField] private GameObject popupManagerGameObject; 
+
+    void Start()
+    {
+        hasBeenInactive = false;
+        if (popupManagerGameObject != null) {
+            popupManagerGameObject.SetActive(false);
+        }
+    }
+
+    void Update() {
+
+        //if (hasBeenInactive == false) {
+            if (Input.anyKey || Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0) {
+                inactivityTimer = 0f;
+                if (popupManagerGameObject != null) {
+                    popupManagerGameObject.SetActive(false);
+                }
+            }
+            else {
+                inactivityTimer += Time.deltaTime;
+            }
+            if (inactivityTimer >= secondsToWait && popupManagerGameObject != null && !popupManagerGameObject.activeSelf) {
+                popupManagerGameObject.SetActive(true);
+            }
+            hasBeenInactive = true;
+        //}//else {
+            //return;
+        //}
+        
+    }
+}
