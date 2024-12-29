801f6e96c172c8c8da9fad353ea69dbf6ad6c7de
Aergu <josaro8260@gmail.com>
2024-12-29 22:06:54 +0100

    updated the EnemyRotateToPlayer script

--- Assets/Scripts/Enemy/EnemyRotateToPlayer.cs
+++ Assets/Scripts/Enemy/EnemyRotateToPlayer.cs
@@ -1,17 +1,50 @@
+using System;
 using UnityEngine;
 
 public class EnemyRotateToPlayer : MonoBehaviour
 {
-    [SerializeField]
-  public Transform Player;
+    private Transform target;
+    private EnemyAttackController attacker;
+    public void Awake()
+    {
+        if (target == null)
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            if (player != null)
+            {
+                target = player.transform;
+            }
+        }
+        attacker = GetComponent<EnemyAttackController>(); 
+    }
+
+    
  
     void Update()
     {
-     	Vector3 direction = Player.position - transform.position;
-         
-         direction.y = 0;
-         
-         transform.rotation = Quaternion.LookRotation(direction);
+     	
    
+    }
+
+    private void RotateToPlayer()
+    {
+       if(IsEnemyNotAttacking())
+        {
+            Vector3 direction = target.position - transform.position;
+         
+            direction.y = 0;
+         
+            transform.rotation = Quaternion.LookRotation(direction);
+        }
+        else
+        {
+            return;
+        }
+        
+    }
+
+    private bool IsEnemyNotAttacking()
+    {
+        return !attacker.IsAttacking;
     }
 }
