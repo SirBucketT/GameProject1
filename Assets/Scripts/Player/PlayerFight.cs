6d562d0b6201d3c90bfaf2bb18b120ce3e290fc4
Sixten Björling <sixteen610@gmail.com>
2025-01-02 16:28:18 +0100

    Update PlayerFight.cs

--- Assets/Scripts/Player/PlayerFight.cs
+++ Assets/Scripts/Player/PlayerFight.cs
@@ -1,38 +1,43 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerFight : MonoBehaviour
 {
     private Animator _animator;
+    public GameObject _leftFoot;
+    private Collider _leftFootCollider;
 
     void Start()
     {
         _animator = GetComponent<Animator>();
+        _leftFoot = GameObject.FindGameObjectWithTag("Left Foot");
+        _leftFootCollider = _leftFoot.GetComponent<Collider>();
         _animator.SetBool("IsAlive", true);
     }
+
     void Update()
-    {   
+    {
         if (Input.GetKeyDown(KeyCode.Q))
         {
             _animator.SetTrigger("Dab");
         }
         if (Input.GetKeyDown(KeyCode.W))
         {
-            _animator.SetTrigger("Kick"); 
+            _animator.SetTrigger("Kick");
+            if (_leftFootCollider != null)
+            {
+                Debug.Log("Foten har en collider!");
+            }
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
             _animator.SetTrigger("Slash");
-
-        }    
-        
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            _animator.SetTrigger("JumpSpin");
         }
-        
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && collision.gameObject == _leftFoot)
+        {
+            Debug.Log("Foten träffade en fiende!");
+        }
     }
 }
