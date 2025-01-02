433880675667f994b741f6031b26f6679d93d0a7
CyborgDitz <sebastian.ditz@student.forsbergsskola.se>
2025-01-02 08:23:08 +0100

    Revert Revert test

--- Assets/Test/Weapons/LongSword.cs
+++ Assets/Test/Weapons/LongSword.cs
@@ -1,36 +1,49 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class LongSword : MonoBehaviour
 {
-    EnemyHealthManager _enemyHealth;
+    private Collider _weaponCollider;
 
-    private PlayerHealth _playerHealth;
-
-    private PlayerData _playerData;
-    private SO_EnemyData _enemyData;
-    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        DisableWeaponCollision();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    void OnCollisionEnter(Collision collision)
+    private void EnableWeaponCollision()
     {
-        if (collision.body.CompareTag("Enemy"))
+        if (_weaponCollider)
         {
-            _enemyHealth.TakeDamage(_playerData.SwordAttack);
-        }
-        if (collision.body.CompareTag("Player"))
-        {
-            _playerHealth.PlayerTakeDamage(_enemyData.GetEnemyDamage);
+            _weaponCollider.enabled = true;
         }
     }
+
+    private void DisableWeaponCollision()
+    {
+        if (_weaponCollider)
+        {
+            _weaponCollider.enabled = false;
+        }
+
+    }
+
+    public void OnEnable()
+    {
+        EnableWeaponCollision();
+    }
+
+    public void OnDisable()
+    {
+        DisableWeaponCollision();
+    }
+
+
 }
